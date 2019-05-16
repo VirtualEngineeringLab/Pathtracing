4477b971ba579cc26898351a96811cd3525ceeac
VEL_US <49537009+VirtualEngineeringLab@users.noreply.github.com>
2019-05-16 09:07:19 -0700

    Initial

---
+++ Assets/RayTracingObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            RayTracingMaster.UpdateObject(transform);
+            print("The transform has changed!");
+            transform.hasChanged = false;
+        }
+    }
+}
