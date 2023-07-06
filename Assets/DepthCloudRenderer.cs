ffd1608851e68afa72713d0d81a0051eabd9cecb
William Sokol Erhard <WilliamSokolErhard@users.noreply.github.com>
2023-07-05 21:22:13 -0700

    VFX working

--- Assets/DepthCloudRenderer.cs
+++ Assets/DepthCloudRenderer.cs
@@ -10,11 +10,13 @@ public class DepthCloudRenderer : MonoBehaviour
     public RenderTexture _renderTexture;
     public Material _renderTextureMat;
     VisualEffect _vFX;
-    int _width = 1000;//3840;
-    int _height = 1;//2160;
+    public int _width = 3840;
+    public int _height = 3840;
 
     bool isDirty = false;
     int particleCount;
+
+    // Texture2D tex;
 
     // Start is called before the first frame update
     void Start()
@@ -25,22 +27,30 @@ public class DepthCloudRenderer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 
         // if(isDirty)
         if(_renderTextureMat.mainTexture!=null)
         {
-            isDirty =false;
+            // RenderTexture.active = (RenderTexture)_renderTextureMat.mainTexture;
+            // tex = new Texture2D(_width, _height, TextureFormat.RGB24, false);
+            // Rect rectReadPicture = new Rect(0, 0, _width, _height);
+            // tex.ReadPixels(rectReadPicture, 0, 0);
+            // tex.Apply();
+            // RenderTexture.active = null; 
+            // isDirty =false;
             // _renderTexture= _renderTextureMat.mainTexture;
             // particleCount = (int)(_renderTexture.height*_renderTexture.width);
-            _vFX.Reinit();
+            // _vFX.Reinit();
             _vFX.SetUInt(Shader.PropertyToID("PixelCount"), (uint)particleCount);
-            // _vFX.SetTexture(Shader.PropertyToID( "texColor"), _textureColor);
+            // _vFX.SetTexture(Shader.PropertyToID( "texColor"), tex);
             _vFX.SetTexture(Shader.PropertyToID("texColor"), _renderTextureMat.mainTexture);
             // _vFX.SetTexture(Shader.PropertyToID( "texScale"), _textureScale);
             _vFX.SetUInt(Shader.PropertyToID("Height"), (uint)_height);
             _vFX.SetUInt(Shader.PropertyToID("Width"), (uint)_width);
+            // Destroy(tex);
         }
 
-        Rasterize();//_renderTextureMat.mainTexture.ToTexture2D());
+        // Rasterize();//_renderTextureMat.mainTexture.ToTexture2D());
     }
 
     public void Rasterize(){
@@ -50,7 +60,7 @@ public class DepthCloudRenderer : MonoBehaviour
         int count = 0;
         Vector3[] positions = new Vector3[particleCount]; 
         Color[] colors = new Color[particleCount];
-        for(int i = 0; i<1; i++){
+        for(int i = 0; i<_height; i++){
             for(int j = 0; j<_width; j++){
                 positions[count] = cam.position + cam.forward+cam.right*0.01f*j+cam.forward+cam.up*0.01f*i;//RenderImage.GetPixel(i,j);
                 colors[count++] = Color.red;//RenderImage.GetPixel(i,j);
